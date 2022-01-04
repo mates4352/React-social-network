185dcb0dd15ca7572f6f6e839530be719ec45b2e
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-04 14:22:02 +0300

    Add styleÐ

--- src/shared/shared_components/Header/Header.tsx
+++ src/shared/shared_components/Header/Header.tsx
@@ -2,6 +2,23 @@ import s from './Header.module.scss'
 
 export const Header = () => {
    return (
-      <header className={s.header}></header>
+      <header className={s.header}>
+         <div className={s.container}>
+            <img src="" alt="Logo image"/>
+
+            <div className={s.content}>
+               <div className={s.theme}>
+                  <input className={s.theme_input} id="theme" type="checkbox"/>
+                  <label className={s.theme_lable} htmlFor="theme"></label>
+               </div>
+
+               <div className={s.account}>
+                  <button className={s.avatar} type="button">
+                     <img src="" alt="Avatar"/>
+                  </button>
+               </div>
+            </div>
+         </div>
+      </header>
    );
 }
