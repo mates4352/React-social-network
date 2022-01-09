ffa02ecfd2c17bf96173617a20308b3778d9df87
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-09 14:32:22 +0300

    Fix style in component app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import s from './App.module.scss';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import {Header} from "./shared/shared_components/Header/Header";
 import {Menu} from "./shared/shared_components/Menu/Menu";
 import {Footer} from "./shared/shared_components/Footer/Footer";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Account} from "./features/account/Account";
 import {Messages} from "./features/messages/Messages";
 import {Music} from "./features/music/Music";
 import {Settings} from "./features/settings/Settings";
 import {News} from "./features/news/News";
 import {Help} from "./features/help/Help";
+
+import s from './App.module.scss';
 
 const App = () => {
    return (
