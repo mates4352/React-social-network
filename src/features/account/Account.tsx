cefc3ed59fef04907b7513adaec343ed80ef3ff1
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-09 15:28:12 +0300

    Add content in component account and style

--- src/features/account/Account.tsx
+++ src/features/account/Account.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import s from './Account.module.scss'
+import imageAvatar from './../../assets/images/avatar.jpg'
 
 type accountType = {};
 
 export const Account = (props: accountType) => {
    return (
-       <div>
-          Account
+       <div className={s.account}>
+          <img className={s.banner} src="#" alt="Изображение баннера"/>
+
+          <div className={s.info}>
+             <img className={s.avatar} src={imageAvatar} alt="Изображение аватара"/>
+
+             <div className={s.description}>
+                <h2 className={s.name}>Name</h2>
+                <p className={s.text}>info</p>
+             </div>
+          </div>
+
+          <div className={s.content}>
+             <div className={s.entry_field}>
+                <textarea className={s.textarea} maxLength={120} placeholder='Write a post...'></textarea>
+                <button className={s.button}>Add post</button>
+             </div>
+          </div>
        </div>
    );
 };
