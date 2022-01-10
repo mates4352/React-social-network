dc6f927b851869af15839b6eab794774aa22b969
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-10 23:49:08 +0300

    Add local component for account

--- src/features/account/Account.tsx
+++ src/features/account/Account.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import s from './Account.module.scss'
 import imageAvatar from './../../assets/images/avatar.jpg'
 import background from './../../assets/images/bg_account.jpg'
-
+import {datePostType, Post} from "./post/post";
 type accountType = {};
+
+const post: Array<datePostType>  = [
+   {
+      id: 0,
+      text: "lorem lorem lorem lorem lorem",
+      time: "2022-01-10",
+   },
+
+   {
+      id: 0,
+      text: "text",
+      time: "2022-01-10",
+   }
+]
 
 export const Account = (props: accountType) => {
    return (
@@ -28,7 +42,7 @@ export const Account = (props: accountType) => {
              </form>
 
              <div className={s.content}>
-
+                {post.map( post => <Post post={post}/>)}
              </div>
           </div>
        </div>
