1b4b69622cbc482c8a1582bad07b206eab84a161
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-11 22:40:49 +0300

    Add layout and style in component Message

--- src/features/messages/Messages.tsx
+++ src/features/messages/Messages.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import s from './Messages.module.scss'
+import {NavLink} from "react-router-dom";
 
 type messagesType = {};
 
-export const Messages = (props: messagesType) => {
+type dataDialogsType = {
+   id: number,
+   name: string,
+}
+
+export const Messages: React.FC<messagesType> = (props) => {
+   const classLink = ({isActive}: any) => isActive ? `${s.link} ${s.link_active}` : s.link;
+
+   const dataDialogs:Array<dataDialogsType> = [
+      {id: 0, name: "Sergey"},
+      {id: 1, name: "Sergey"},
+      {id: 2, name: "Sergey"},
+      {id: 3, name: "Sergey"},
+      {id: 4, name: "Sergey"},
+      {id: 5, name: "Sergey"},
+   ]
+
    return (
-       <div>
-          Messages
+       <div className={s.messages}>
+          <div className={s.container}>
+             <h2 className={s.title}>Dialogs</h2>
+             <ul className={s.Dialogs}>
+                {dataDialogs.map( dialogs =>
+                    <li className={s.name}>
+                       <NavLink className={classLink} to={`Messages/${dialogs.id}`}>{dialogs.name}</NavLink>
+                    </li>
+                )}
+             </ul>
+          </div>
        </div>
    );
 };
