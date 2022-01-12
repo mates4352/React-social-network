2d4506d776b729227266d1f46960d8b83819de8d
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-12 15:07:44 +0300

    Add layout and style in page Communication

--- src/features/сommunication/Сommunication.tsx
+++ src/features/сommunication/Сommunication.tsx
@@ -22,30 +22,61 @@ export const Сommunication: React.FC<messagesType> = (props) => {
       {id: 5, name: "Sergey"},
    ]
 
-   const dataMassage:Array<dataMassageType> = [
+   const StrangerDataMassage:Array<dataMassageType> = [
       {id: 0, text: "Hello"},
+      {id: 1, text: "Hello"},
+      {id: 1, text: "Hello"},
       {id: 1, text: "Hello"},
    ]
 
+   const MyDateMassage:Array<dataMassageType> = [
+      {id: 0, text: "Bay"},
+      {id: 1, text: "Bay"},
+      {id: 1, text: "Bay"},
+      {id: 1, text: "Bay"},
+   ]
+
+
    return (
        <div className={s.communication}>
-          <div className={s.container}>
-             <h2 className={s.title}>Dialogs</h2>
+          <h1 className={s.title}>Dialogs</h1>
+
+          <div className={s.contact}>
+             <input className={s.search} type="text" placeholder="Search contact..."/>
+
              <ul className={s.dialogs}>
                 {dataDialogs.map( dialogs =>
-                    <li className={s.name}>
+                    <li className={s.dialogsItem}>
                        <NavLink className={classLink} to={`Messages/${dialogs.id}`}>{dialogs.name}</NavLink>
                     </li>
                 )}
              </ul>
+          </div>
 
-             <ul className={s.list_massage}>
-                {dataMassage.map( massage =>
-                    <li className={s.item_massage} key={massage.id}>
+          <div className={s.content}>
+             <p className={s.contactName}>
+                name
+             </p>
+
+             <ul className={s.list}>
+                {StrangerDataMassage.map( massage =>
+                    massage &&
+                    <li className={s.item} key={massage.id}>
                        <Massage massage={massage}/>
                     </li>
                 )}
+
+                {MyDateMassage.map( massage =>
+                    massage &&
+                   <li className={s.item} key={massage.id}>
+                     <Massage massage={massage}/>
+                   </li>
+                )}
              </ul>
+
+             <form className={s.entryField}>
+                <input className={s.entryFieldInput} type="text" placeholder="To write a message..."/>
+             </form>
           </div>
        </div>
    );
